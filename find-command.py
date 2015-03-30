a725a75b632a2a6e1165e82e790e25c82058b3ae
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-03-30 00:35:26 -0500

    Fixed up basic search to new invocation hierarchy.

--- find-command.py
+++ find-command.py
@@ -8,6 +8,8 @@ with open("command-database.json") as db_file:
     commands = json.load(db_file)
 
 for command in commands:
-    command_string = command['invocation']['string']
-    if match_string in command_string:
-        print command_string
+    invocations = command['invocations'].keys()
+    for shell in invocations:
+        command_string = command['invocations'][shell]['string']
+        if match_string in command_string:
+            print command_string
