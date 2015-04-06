b54f13c91ad58876f1b42822b8a51bffb5ea2ebe
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-04-06 01:05:21 -0500

    Outline for adding new commands.

---
+++ command-skeleton.js
@@ -0,0 +1,49 @@
+[
+    {
+        "component-command-info": {
+            "bash-types": {
+            },
+            "debian-packages": {
+            },
+            "debian-paths": {
+            },
+            "requirements": {
+                "command-name": {
+                    "internet-connection": "no",
+                    "sudo": "no"
+                }
+            }
+        },
+        "component-commands": [
+        ],
+        "description": {
+            "bytes": "",
+            "sha1hex": "",
+            "string": ""
+        },
+        "invocations": {
+            "POSIX" : {
+                "bytes": "",
+                "changeable-arguments": {
+                    "arg1": {
+                        "component-command": "",
+                        "component-command-flag": "",
+                        "description-string": "",
+                        "invocation-slice": [],
+                        "type": ""
+                    }
+                },
+                "compatible-shells": [
+                ],
+                "incompatible-shells": [
+                ],
+                "sha1hex": "",
+                "string": "command"
+            }
+        },
+        "relevant-urls": [
+        ],
+        "side-effects": {
+        }
+    }
+]
