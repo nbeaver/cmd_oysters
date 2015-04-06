8a530e0b1fe9290a40eed4dcf2003795d7ac72be
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-04-06 17:56:18 -0500

    Add command flags, but have not implemented the actions they produce.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ validate_json : command-database.json Makefile
 	json_verify < command-database.json
 
 test_python_script : find-command.py Makefile
-	python find-command.py "ping"
+	python find-command.py -s "ping"
 
 lint_database : command-database.json lint-database.py Makefile
 	python lint-database.py command-database.json
--- find-command.py
+++ find-command.py
@@ -1,17 +1,25 @@
 #! /usr/bin/env python
 import json
 import sys
+import argparse
 
-match_string = ''.join(sys.argv[1:])
+parser = argparse.ArgumentParser(description='search for shell commands')
+parser.add_argument('-c', '--commands', help='component command search', required=False, nargs='+')
+parser.add_argument('-s', '--substring', help='simple substring search', required=False)
+parser.add_argument('-t', '--token-substring', help='unordered token subset search',required=False, nargs='+')
+parser.add_argument('-d', '--database', help='path to database file', required=False, default="command-database.json")
+args = parser.parse_args()
 
-with open("command-database.json") as db_file:
+with open(args.database) as db_file:
     commands = json.load(db_file)
 
 # TODO: take --commands argument and search in component commands.
+
+print args
 
 for command in commands:
     invocations = command['invocations'].keys()
     for shell in invocations:
         command_string = command['invocations'][shell]['string']
-        if match_string in command_string:
+        if args.substring in command_string:
             print command_string
