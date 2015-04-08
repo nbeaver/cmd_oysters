5d4a81eaa3a193b01816075b5a306dcac322ad3c
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-04-07 20:09:33 -0500

    Refactor for clarity.

--- check-pseudoschema.py
+++ check-pseudoschema.py
@@ -10,37 +10,38 @@ if len(sys.argv) == 1:
 with open(sys.argv[1]) as db_file:
     commands = json.load(db_file)
 
-def generic_child(child_list):
+def wildcard(child_name):
     # If the child starts with '$',
     # e.g. "$COMMAND",
-    # it is generic.
-    if len(child_list) == 1 and child_list[0][0] == '$':
-        return child_list[0]
+    # it is a wildcard.
+    if child_name[0] == '$':
+        return True
     else:
-        return None
+        return False
 
-def check_pseudoschema(parent, directory, trace_path=""):
+def check_pseudoschema(parent_json, parent_pseudoschema, trace_path=""):
     try:
-        children = parent.keys()
+        children_json = parent_json.keys()
     except AttributeError:
+        # Stop recursing, since the JSON object doesn't have child objects.
         return
 
-    known_children = os.listdir(directory)
-    for child in children:
-
-        new_parent = parent[child]
-
-        if generic_child(known_children):
-            new_path = os.path.join(directory,generic_child(known_children))
-        else:
-            new_path = os.path.join(directory, child)
-        
-        if child in known_children or generic_child(known_children):
-            check_pseudoschema(new_parent, new_path, trace_path+":"+child)
-        else:
-            # TODO: should this halt the script or not?
-            raise ValueError, "Input does not match pseudoschema: `"+trace_path+":"+child+"' not in "+directory
-            # TODO: Can this trace back to the line of the original JSON file?
+    children_pseudoschema = os.listdir(parent_pseudoschema)
+    if len(children_pseudoschema) == 1 and wildcard(children_pseudoschema[0]):
+        # This is a wildcard in the schema, so just continue recursing.
+        for child_json in children_json:
+            next_path = os.path.join(parent_pseudoschema, children_pseudoschema[0])
+            check_pseudoschema(parent_json[child_json], next_path, trace_path+":"+child_json)
+    else:
+        for child_json in children_json:
+            # We're only checking that the JSON child objects are in the pseudoschema;
+            # if the JSON doesn't have all of the pseudoschema, that's ok.
+            if child_json in children_pseudoschema:
+                next_path = os.path.join(parent_pseudoschema, child_json)
+                check_pseudoschema(parent_json[child_json], next_path, trace_path+":"+child_json)
+            else:
+                raise ValueError, "Input does not match pseudoschema: `"+trace_path+":"+child_json+"' not one of "+str(children_pseudoschema)+" in "+parent_pseudoschema
+                # TODO: Figure out a way to trace back to the line of the original JSON file. Will be hard, since we've already parsed the JSON.
 
 for command in commands:
     check_pseudoschema(command, "pseudo-schema/")
