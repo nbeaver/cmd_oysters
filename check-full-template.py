1aad2449fbb2fe3d095bfaab3a441e09855e5d38
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-04-08 21:01:39 -0500

    Correctly check the entire pseudo-schema is present in full-command-template.

--- check-full-template.py
+++ check-full-template.py
@@ -7,22 +7,20 @@ if len(sys.argv) == 1:
     print "Usage: python check-full-template.py full-command-template.json pseudo-schema"
     sys.exit(1)
 
-with open(sys.argv[1]) as json_file:
-    commands = json.load(json_file)
+json_file = sys.argv[1]
+commands = json.load(open(json_file))
     
 pseudoschema_root = sys.argv[2]
 if not os.path.isdir(pseudoschema_root):
     print "`"+pseudoschema_root+"' is not a directory or does not exist."
     sys.exit(1)
 
-def validate(directory, json_object, trace):
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-                assert filename in json_object.keys(), os.path.join(trace,filename)+" is not in "+str(json_object.keys())
-
-        for dirname in dirnames:
-            assert dirname in json_object.keys()
-            validate(dirname, json_object[dirname], os.path.join(trace, dirname))
+def validate(path, json_object):
+    for child in os.listdir(path):
+        child_path = os.path.join(path, child)
+        assert child in json_object.keys(), child_path+" not in "+json_file+"\nkeys: "+str(json_object.keys())
+        if os.path.isdir(child_path):
+            validate(child_path, json_object[child])
 
 for command in commands:
-    validate(pseudoschema_root, command, pseudoschema_root)
+    validate(pseudoschema_root, command)
