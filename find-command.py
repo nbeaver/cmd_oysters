c78e8ebb06887370bc8a5b94da53bd474eafdacc
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-04-15 02:25:42 -0500

    Add a description token search.

--- find-command.py
+++ find-command.py
@@ -3,13 +3,14 @@ import json
 import os
 import sys
 import argparse
+import string
 
 parser = argparse.ArgumentParser(description='search for shell commands')
 parser.add_argument('-c', '--commands', help='component command search', required=False, nargs='+')
 parser.add_argument('-s', '--substring', help='simple command substring search', required=False)
 parser.add_argument('-t', '--tokens', help='unordered token subset command search',required=False, nargs='+')
 parser.add_argument('-d', '--description', help='command description', required=False)
-# parser.add_argument('-D', '--description-tokens', help='description token search', required=False)
+parser.add_argument('-D', '--description-tokens', help='description token search (case-insensitive)', required=False, nargs='+')
 
 default_json_file = os.path.join(sys.path[0],"command-database.json") # need to do it this way for symlinks to work.
 parser.add_argument('-j', '--json', help='path to JSON input file', required=False, default=default_json_file)
@@ -23,15 +24,29 @@ if len(sys.argv) < 2:
 with open(args.json) as db_file:
     commands = json.load(db_file)
 
+def tokens(text):
+    return text.encode('utf-8').translate(None, string.punctuation).split()
+
+def lowercase_subset(A, B):
+    set_a = set([a.lower() for a in A])
+    set_b = set([b.lower() for b in B])
+    return set_a.issubset(set_b)
+
 for command in commands:
     if args.commands:
         if not set(args.commands).issubset(set(command['component-commands'])):
             # The command list is not a subset of the component commands,
             # so try next command.
             continue
+
     if args.description:
         if not args.description in command['description']['string']:
             continue
+
+    if args.description_tokens:
+        if not lowercase_subset(args.description_tokens, tokens(command['description']['string'])):
+            continue
+
     for invocation in command['invocations'].keys():
         command_string = command['invocations'][invocation]['string']
         if args.substring:
