736f882337cd1cf98ab6a1490ed071c615792ee1
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-04-17 00:17:39 -0500

    Remove unnecessary files.

--- split-db.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-commands = json.load(open("command-database.json"))
-for command in commands:
-    f = open("command-database/"+command['description']['sha1hex']+".json", 'w')
-    json.dump(command, f, indent=4, sort_keys=True)
-    f.close()
