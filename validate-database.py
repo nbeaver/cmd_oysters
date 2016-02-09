016fba55e038cf7d27796d599c35288971cd40a4
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-08 23:04:26 -0600

    Take out superfluous SHA1s.

--- validate-database.py
+++ validate-database.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python2
 import json
 import jsonschema
-import hashlib
 import sys
 import os
 import glob
@@ -47,7 +46,7 @@ def find_slice(string, substring):
         stop = start + len(substring)
         return [start, stop]
 
-def validate_command(command, description_SHA1_from_filename):
+def validate_command(command, uuid_from_filename):
     global modify_file
 
     def assert_custom(assertion, error_string):
@@ -58,48 +57,11 @@ def validate_command(command, description_SHA1_from_filename):
             traceback.print_exc()
             raise
 
-    def assert_custom_warn_only(assertion, error_string):
-        if assertion:
-            return
-        else:
-            sys.stderr.write("Warning in file: "+ json_filepath+'\n')
-            sys.stderr.write(error_string+'\n')
-            pass
-
     def assert_subset(subset, superset):
         assert_custom(subset.issubset(superset), repr(subset)+ "is not a subset of " + repr(superset))
 
     def assert_in(A, B):
         assert_custom(A in B, repr(A)+ "is not in " + repr(B))
-
-    def match_sha1(string, nominal_sha1):
-        calculated_sha1 = hashlib.sha1(string).hexdigest()
-        if nominal_sha1 != calculated_sha1:
-            assert_custom_warn_only(False, \
-                "SHA1s do not match:\n%s (in file)\n%s (calculated)\n%r (command representation)" \
-                % (nominal_sha1, calculated_sha1, string))
-            return False
-        else:
-            return True
-
-    def supply_sha1(string):
-        assert_custom_warn_only(False, \
-            "Warning: No SHA1 for `" + string + "'\n"+\
-            "Should be: "+hashlib.sha1(string).hexdigest()+"\n")
-
-    def check_sha1_dict(dict_to_check, require_unique=True):
-        global modify_file
-        try:
-            if match_sha1(dict_to_check['string'], dict_to_check['sha1-hex']):
-                if require_unique:
-                    unique_SHA1s.add(dict_to_check['sha1-hex'])
-            else:
-                if modify_file:
-                    dict_to_check['sha1-hex'] = hashlib.sha1(dict_to_check['string']).hexdigest()
-        except KeyError:
-            supply_sha1(dict_to_check['string'])
-            if modify_file:
-                dict_to_check['sha1-hex'] = hashlib.sha1(dict_to_check['string']).hexdigest()
 
     if 'component-command-info' in command.keys():
 
@@ -113,23 +75,13 @@ def validate_command(command, description_SHA1_from_filename):
                         # e.g. `ls` is in `/bin/ls`
                         assert_in(command_name, info_item['executable-path'])
 
-    if 'relevant-urls' in command.keys():
-        for url in command['relevant-urls']:
-            # We don't add check these SHA-1 hashe for uniqueness
-            # because two different commands might link to the same URI,
-            # and that's ok.
-            check_sha1_dict(url, require_unique=False)
-
-    check_sha1_dict(command['description'])
-
     uuids.add(command['uuid'])
 
-    assert_custom_warn_only(description_SHA1_from_filename == hashlib.sha1(command['description']['string']).hexdigest(),
-        "Filename does not match SHA1 of description.\n" + \
-        "Should be: " + str(hashlib.sha1(command['description']['string']).hexdigest()) + ".json")
+    if uuid_from_filename != command['uuid']:
+            sys.stderr.write("Warning in file: "+ json_filepath+'\n')
+            sys.stderr.write("Filename does not match UUID. Should be: {}.json\n".format(command['uuid']))
 
     def validate_invocation(invocation):
-        check_sha1_dict(invocation)
 
         if 'changeable-arguments' in invocation_dict.keys():
             arg_dict = invocation_dict['changeable-arguments']
@@ -168,7 +120,6 @@ if len(sys.argv) == 1:
     sys.stderr.write("Usage: python "+sys.argv[0]+" path-to-json-files/"+'\n')
     sys.exit(1)
 
-unique_SHA1s = NoDuplicates()
 uuids = NoDuplicates()
 num_invocations = 0
 
