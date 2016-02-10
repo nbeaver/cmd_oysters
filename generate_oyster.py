d7816cf470a7bafdfa0194a2664b2ad11e87c0b0
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-10 17:15:01 -0600

    Help it find the directories now that it's up one.

--- generate_oyster.py
+++ generate_oyster.py
@@ -6,15 +6,15 @@ import sys
 import json
 import uuid
 
-parent, _ = os.path.split(sys.path[0])
-print('parent =', parent)
-template_path = os.path.join(parent, 'templates', 'simple.json')
+root = sys.path[0]
+print('root =', root)
+template_path = os.path.join(root, 'templates', 'simple.json')
 with open(template_path) as template:
     oyster = json.load(template)
 
 new_id = str(uuid.uuid4())
 new_filename = new_id + '.json'
-new_filepath = os.path.join(parent, 'cmdoysters', new_filename)
+new_filepath = os.path.join(root, 'cmdoysters', new_filename)
 with open(new_filepath, 'w') as new_file:
     oyster['uuid'] = new_id
     json.dump(oyster, new_file, indent=4, separators=(',', ': '), sort_keys=True)
