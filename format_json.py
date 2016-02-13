83ba4e1eedaa3c43faef8d3c622ab84fccc6f10a
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-12 20:08:38 -0600

    Better error handling and portability.

--- format_json.py
+++ format_json.py
@@ -5,6 +5,11 @@ import json
 
 for filepath in sys.argv[1:]:
     with open(filepath) as f:
-        oyster = json.load(f)
+        try:
+            oyster = json.load(f)
+        except ValueError:
+            sys.stderr.write("In file: {}\n".format(filepath))
+            raise
     with open(filepath, 'w') as f:
         json.dump(oyster, f, indent=4, separators=(',', ': '), sort_keys=True)
+        f.write('\n') # add a trailing newline.
