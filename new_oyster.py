388cd7407ab8aa14a00f46b52269116510dda95a
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2020-02-21 20:41:25 -0500

    Do not run on import.

--- new_oyster.py
+++ new_oyster.py
@@ -11,41 +11,43 @@ def get_year():
     now = datetime.datetime.now()
     return now.year
 
-if len(sys.argv) > 1:
-    invocation = sys.argv[1]
-else:
-    sys.stderr.write("Usage: python "+sys.argv[0]+" 'command-invocation'"+'\n')
-    sys.exit(1)
+if __name__ == '__main__':
 
-new_uuid = str(uuid.uuid4())
+    if len(sys.argv) > 1:
+        invocation = sys.argv[1]
+    else:
+        sys.stderr.write("Usage: python "+sys.argv[0]+" 'command-invocation'"+'\n')
+        sys.exit(1)
 
-oyster = \
-{
-    "component-commands": [
-        "<FIXME>"
-    ],
-    "copying": {
-        "authors": [
+    new_uuid = str(uuid.uuid4())
+
+    oyster = \
+    {
+        "component-commands": [
             "<FIXME>"
         ],
-        "license-name": "MIT (Expat) License",
-        "license-url": "http://opensource.org/licenses/MIT",
-        "year": get_year()
-    },
-    "description": {
-        "verbose-description": "<FIXME>"
-    },
-    "invocations": [
-        {
-            "invocation-string": invocation
-        }
-    ],
-    "uuid": new_uuid
-}
+        "copying": {
+            "authors": [
+                "<FIXME>"
+            ],
+            "license-name": "MIT (Expat) License",
+            "license-url": "http://opensource.org/licenses/MIT",
+            "year": get_year()
+        },
+        "description": {
+            "verbose-description": "<FIXME>"
+        },
+        "invocations": [
+            {
+                "invocation-string": invocation
+            }
+        ],
+        "uuid": new_uuid
+    }
 
-root = sys.path[0]
-new_filename = new_uuid + '.json'
-new_filepath = os.path.join(root, 'cmdoysters', new_filename)
-with open(new_filepath, 'w') as new_file:
-    json.dump(oyster, new_file, indent=4, separators=(',', ': '), sort_keys=True)
-    print('Created new CmdOyster:\n{}'.format(new_filepath))
+    root = sys.path[0]
+    new_filename = new_uuid + '.json'
+    new_filepath = os.path.join(root, 'cmdoysters', new_filename)
+    with open(new_filepath, 'w') as new_file:
+        json.dump(oyster, new_file, indent=4, separators=(',', ': '), sort_keys=True)
+        print('Created new CmdOyster:\n{}'.format(new_filepath))
